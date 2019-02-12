ea144138cb298fc98d3c7bebff048b3f8aaa9fc4
Nalaka Manathunga <manathunga.nalaka@izettle.com>
2019-02-12 21:44:52 +0100

    Configuring middleware

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,18 @@ const PORT:number = Number(process.env.SERVER_PORT) || 3000
 // BAse config this can be chnages bsed on the env
 const baseConfig = {
   ENV,
-  PORT
+  PORT,
+  corsConfig: {
+    origin: "*",
+    exposeHeaders: ["Authorization"],
+    credentials: true,
+    allowMethods: ["GET", "PUT", "POST", "DELETE"],
+    allowHeaders: ["Authorization", "Content-Type"],
+    keepHeadersOnError: true,
+  },
+  bodyParserConfig: {
+    enableTypes: ["json"]
+  }
 }
 
 export default baseConfig
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import Koa from "Koa"
 import logger from 'koa-logger'
+import responseTime from 'koa-response-time'
+import helmet from 'koa-helmet'
+import cors from 'kcors'
+import bodyParser from 'koa-bodyparser'
 
 import config from "./config"
 
 const app = new Koa()
 
+app.use(responseTime())
 app.use(logger())
+app.use(helmet())
+app.use(cors(config.corsConfig))
+app.use(bodyParser(config.bodyParserConfig))
+
 
 app.use((ctx: Koa.Context) => {
   ctx.body = 'This from the koa ts app'
@@ -14,11 +23,11 @@ app.use((ctx: Koa.Context) => {
 const startServer =  async () => {
   try {
       await app.listen(config.PORT, () => {
-        console.log('Server listing on port 3000')
+        console.log(`Server listing on port ${config.PORT}`)
       })
   } catch (error) {
     console.log(error)
   }
 }
 
-startServer()
+export default startServer
